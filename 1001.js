957074c5ecab587ade7c2c9f0f66fed0fd7b3a30
Josiany Rodrigues da Silva <109697430+josianyrs@users.noreply.github.com>
2023-02-20 11:17:25 -0300

    Update 1001.js

--- 1001.js
+++ 1001.js
@@ -1,4 +1,4 @@
-//*
+/*
 Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo especificado e não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
 
 Entrada
@@ -6,8 +6,8 @@ A entrada contém 2 valores inteiros.
 
 Saída
 Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha. Cuide para que tenha um espaço antes e depois do sinal de igualdade, conforme o exemplo abaixo.
-Valores escolhidos são: 9 e 10.
-*//
+Valores escolhidos são: 9 e 10.*/
+
 var lines = [9,10];
 var A = parseInt(lines.shift()); var B = parseInt(lines.shift());
 console.log("X =", A+B);
